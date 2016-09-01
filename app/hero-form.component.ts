37d028fbb469040014c6ad0f154ea0e8dcdc89db
svidya <svidya1725@gmail.com>
2016-09-01 14:31:40 +0100

    Intial Commit

---
+++ app/hero-form.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { Hero }    from './hero';
+@Component({
+  selector: 'hero-form',
+  templateUrl: 'app/hero-form.component.html'
+})
+export class HeroFormComponent {
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  submitted = false;
+  active = true;
+  onSubmit() { this.submitted = true; }
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+
+  newHero() {
+   this.model = new Hero(42, '', '');
+    this.active = false;
+    setTimeout(() => this.active = true, 0);
+}
+
+}
